653f656da54a01d588f29c35bab33467b19d2e97
Ankit Singh <111173380+SinkAnkit@users.noreply.github.com>
2023-05-14 01:15:53 +0530

    Create CINandCOUT.cpp

---
+++ CINandCOUT.cpp
@@ -0,0 +1,16 @@
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n1,n2,n3,n;
+    cin>>n1>>n2>>n3;
+    n=n1+n2+n3;
+    cout<<n<<endl;
+    return 0;
+}
